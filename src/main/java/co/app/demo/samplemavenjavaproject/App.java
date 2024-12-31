00fbe72210d0964421b04e32479414f3fc92c388
fayaz9985 <f6133942@gmail.com>
2025-01-01 03:40:24 +0530

    sample maven java

---
+++ src/main/java/co/app/demo/samplemavenjavaproject/App.java
@@ -0,0 +1,13 @@
+package co.app.demo.samplemavenjavaproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
